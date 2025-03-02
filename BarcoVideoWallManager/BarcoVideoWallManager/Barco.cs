68b74a75fea7bd92c09a52d230d62b7842b093fa
joeandrewgomez <joeandrewgomez@gmail.com>
2025-03-02 01:59:06 -0500

    Initial code commit

---
+++ BarcoVideoWallManager/BarcoVideoWallManager/Barco.cs
@@ -0,0 +1,64 @@
+﻿using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+
+namespace BarcoVideoWallManager;
+
+public class BarcoVideoWallManager
+{
+
+    public string IpAddress { get; set; }
+    public string Psk { get; set; }
+    public bool Debug { get; set; }
+    private static HttpClient? _httpClient;
+
+    public BarcoVideoWallManager(string ipAddress, string psk, bool debug)
+    {
+        IpAddress = ipAddress;
+        Psk = psk;
+        var handler = new HttpClientHandler { CookieContainer = new CookieContainer() };
+        _httpClient = new HttpClient(handler)
+        {
+            BaseAddress = new Uri($"https://{ipAddress}/api/v1"),
+            Timeout = TimeSpan.FromSeconds(30)
+
+        };
+        _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+    }
+
+    public async Task<bool> AuthenticateAsync()
+    {
+
+        //TODO: Have this called from enum/dict eventually
+        var authPayload = new
+        {
+            type = "REST",
+            key = Psk
+        };
+        var jsonPayload = JsonSerializer.Serialize(authPayload);
+        var content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
+        var response = await _httpClient?.PostAsync("auth/key", content)!;
+        if (response.IsSuccessStatusCode)
+        {
+            if (!Debug) return true;
+            var responseBody = await response.Content.ReadAsStringAsync();
+            Console.WriteLine("Authentication successful: " + responseBody);
+
+            return true; 
+        }
+        else
+        {
+            var errorBody = await response.Content.ReadAsStringAsync();
+            await Console.Error.WriteLineAsync($"Authentication failed with status {response.StatusCode}: {errorBody}");
+            return false;
+        }
+
+
+    }
+
+
+
+
+
+}
