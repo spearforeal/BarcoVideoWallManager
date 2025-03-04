f0302b1928d77b7f4a28a9f2bd8def87116a6947
joeandrewgomez <joeandrewgomez@gmail.com>
2025-03-04 18:13:41 -0500

    seperate enum/dict into class

--- BarcoVideoWallManager/BarcoVideoWallManager/Barco.cs
+++ BarcoVideoWallManager/BarcoVideoWallManager/Barco.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using System.Net.Http.Headers;
+using System.Security.AccessControl;
 using System.Text;
 using System.Text.Json;
 
@@ -9,24 +10,17 @@ public class Barco
 {
 
     public string IpAddress { get; set; }
-    public string Psk { get; set; }
+    private string Psk { get; set; }
     public bool Debug { get; set; }
     private static HttpClient? _httpClient;
+    private readonly CommandDictionary _commands = new();
     private string Sid { get; set; }
 
-    private static readonly Dictionary<Endpoint, string> EndpointMap = new()
-    {
-        { Endpoint.AuthKey, "auth/key" },
-        { Endpoint.WallBrightness, "wall/brightness" }
 
-    };
-    public enum Endpoint
-    {
-        AuthKey,
-        WallBrightness
-        
-        
-    }
+
+
+
+
 
     public Barco(string ipAddress, string psk, bool debug)
     {
@@ -52,9 +46,11 @@ public class Barco
             type = "REST",
             key = Psk
         };
+        var cmd = _commands.GeneralCommands[CommandDictionary.General.Authenticate];
+            
         var jsonPayload = JsonSerializer.Serialize(authPayload);
         var content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
-        var response = await _httpClient?.PostAsync("v1/auth/key", content)!;
+        var response = await _httpClient?.PostAsync(cmd, content)!;
         if (response.IsSuccessStatusCode)
         {
             if (response.Headers.TryGetValues("Set-Cookie", out var cookieValues))
@@ -83,5 +79,95 @@ public class Barco
 
 
 
+
+}
+public class CommandDictionary
+{
+    public enum General
+    {
+        Authenticate,
+        GetVwMVersion,
+        GetApiVersion,
+    }
+
+    public enum Wall
+    {
+        GetWallBrightness,
+        SetWallBrightness,
+        GetAbsoluteWallBrightness, 
+        SetAbsoluteWallBrightness,
+        GetPowerWallState,
+        SetPowerWallState,
+        GetWallPresets,
+        SetWallPreset,
+        GetWallTemperature,
+        GetWallAlert,
+        GetWallSize,
+        GetWallDevice,
+        GetWallHealth,
+        GetWallOsd,
+        SetWallOsd,
+        GetWallName,
+    }
+
+    public enum Device
+    {
+        GetDeviceTemperature,
+        GetDevicesTemperature,
+        GetDeviceFanSpeed,
+        GetDevicesFanSpeed,
+        GetDeviceRuntime,
+        GetDevicesRuntime,
+        GetDeviceHealth,
+        GetDevicesHealth,
+        GetDeviceAlert,
+        GetDevicesAlert,
+        GetDevicesPowerState
+    }
+
+    public Dictionary<General, string> GeneralCommands { get;  } = new()
+    {
+        { General.Authenticate, "auth/key" },
+        { General.GetVwMVersion, "something" },
+        { General.GetApiVersion, "Something" }
+
+    };
+
+    public Dictionary<Wall, string> WallCommands { get; } = new()
+    {
+        { Wall.GetWallBrightness, "Something" },
+        { Wall.SetWallBrightness, "Something" },
+        { Wall.GetAbsoluteWallBrightness, "Something" },
+        { Wall.SetAbsoluteWallBrightness, "Something" },
+        { Wall.GetPowerWallState, "Something" },
+        { Wall.SetPowerWallState, "Something" },
+        { Wall.GetWallPresets, "Something" },
+        { Wall.SetWallPreset, "Something" },
+        { Wall.GetWallTemperature, "Something" },
+        { Wall.GetWallAlert, "Something" },
+        { Wall.GetWallSize, "Something" },
+        { Wall.GetWallDevice, "Something" },
+        { Wall.GetWallHealth, "Something" },
+        { Wall.GetWallOsd, "Something" },
+        { Wall.SetWallOsd, "Something" },
+        { Wall.GetWallName, "Something" },
+
+    };
+
+    public Dictionary<Device, string> DeviceCommands { get; } = new()
+    {
+        { Device.GetDeviceTemperature, "Something" },
+        { Device.GetDevicesTemperature, "Something" },
+        { Device.GetDeviceFanSpeed, "Something" },
+        { Device.GetDevicesFanSpeed, "Something" },
+        { Device.GetDeviceRuntime, "Something" },
+        { Device.GetDevicesRuntime, "Something" },
+        { Device.GetDeviceHealth, "Something" },
+        { Device.GetDevicesHealth, "Something" },
+        { Device.GetDeviceAlert, "Something" },
+        { Device.GetDevicesAlert, "Something" },
+        { Device.GetDevicesPowerState, "Something" },
+
+    };
 
 }
