74079a74d6962d196e882352c46c8bf3c1bbacef
joeandrewgomez <joeandrewgomez@gmail.com>
2025-04-22 00:57:36 -0400

    added summary for power state enum and response class

--- BarcoVideoWallManager/BarcoVideoWallManager/Wall/Properties/WallPowerProperties.cs
+++ BarcoVideoWallManager/BarcoVideoWallManager/Wall/Properties/WallPowerProperties.cs
@@ -1,14 +1,18 @@
 using System.Text.Json.Serialization;
 
 namespace BarcoVideoWallManager;
-
+/// <summary>
+/// Power States: On, Idle, Standby
+/// </summary>
 public enum WallPowerState
 {
     On,
     Idle,
     Standby
 }
-
+/// <summary>
+/// Represents the response from GetPowerStateAsync. Contains the power state as a <see cref="WallPowerState"/> enum value.
+/// </summary>
 public class WallPowerStateResponse
 {
     [JsonPropertyName("power")] public string? Power { get; }
